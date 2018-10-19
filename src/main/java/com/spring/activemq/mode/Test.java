978e4fadd629811999f22e029095269889eb552c
gitliuhongbing <liuhb@guahao.com>
2018-10-19 17:10:51 +0800

    添加单元测试

---
+++ src/main/java/com/spring/activemq/mode/Test.java
@@ -0,0 +1,15 @@
+package com.spring.activemq.mode;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * Created by liuhongbing on 2018/10/19.
+ */
+public class Test {
+
+    public static   void  main(String[] args){
+        JsonResult jr=JsonResult.success("returnCode","returnMessage");
+        String json= JSON.toJSONString(jr);
+        System.out.println(json);
+    }
+}
