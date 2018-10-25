1e6f1e6c0bf38bfb52f2a3b60244299679c7475e
gitliuhongbing <liuhb@guahao.com>
2018-10-25 16:34:43 +0800

    abc

--- src/main/java/com/spring/activemq/controller/TestController.java
+++ src/main/java/com/spring/activemq/controller/TestController.java
@@ -1,10 +1,6 @@
 package com.spring.activemq.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
@@ -12,11 +8,10 @@ import java.util.Date;
 /**
  * Created by liuhongbing on 2018/10/25.
  */
-@Controller
+@RestController
 public class TestController {
 
     @RequestMapping(value = "/test",method = RequestMethod.POST)
-    @ResponseBody
     public  String test(@RequestBody String abc){
         System.out.println(abc);
         return new Date().getTime()+"";
